4a1b02caca110ffa217e443a4f1fc873823d8117
IgnacioBastias <103468121+IgnacioBastias@users.noreply.github.com>
2022-10-05 09:30:55 -0300

    Correccion 2desafio

--- Desafio2/manejoDeArchivos.js
+++ Desafio2/manejoDeArchivos.js
@@ -11,13 +11,41 @@ class Contenedor {
 
     }
 
+    leerArchivo = () => {
+
+        try {
+            const dato = fs.readFileSync(nombreArchivo, `utf-8`);
+            return JSON.parse(dato);
+        }
+        catch {
+            throw new Error('Archivo no existe');
+        }
+
+    }
+
+    getAll = async () => {
+
+        try {
+            const productos = this.leerArchivo()
+            return (productos);
+        }
+        catch {
+            console.log('Sin productos');
+        }
+
+    }
+
     save = async (nuevoProducto) => {
 
-        if (nuevoProducto === undefined) throw new Error('Producto invalido')
+        const productos = this.leerArchivo();
 
-        const dato = await fs.promises.readFile(nombreArchivo, `utf-8`);
-        const productos = JSON.parse(dato);
-        const newId = productos[productos.length - 1].id +1;
+        let newId = 0;
+
+        if (productos.length !== 0) {
+            newId = productos[productos.length - 1].id + 1;
+        } else {
+            newId = 1;
+        }
 
         const producto = {
             title: nuevoProducto.title,
@@ -27,49 +55,64 @@ class Contenedor {
 
         productos.push(producto);
 
-        await fs.promises.writeFile(nombreArchivo, JSON.stringify(productos, null, '\t'));
-
-        return (newId)
+        try {
+            await fs.promises.writeFile(nombreArchivo, JSON.stringify(productos, null, '\t'));
+            return (newId);
+        }
+        catch {
+            throw new Error('Error al guardar');
+        }
     }
 
     getById = async (id) => {
 
-        const dato = await fs.promises.readFile(nombreArchivo, `utf-8`);
-        const productos = JSON.parse(dato);
+        const productos = this.leerArchivo();
 
-        const indice = productos.findIndex((producto) => producto.id === id);
-        if (indice < 0) {
-            throw new Error('No existe')
+        try {
+            const indice = productos.findIndex((producto) => producto.id === id);
+
+            if (indice !== -1) {
+                productos.splice(indice, 1);
+                return (productos[indice]);
+            } else {
+                console.log("El producto no existe");
+            }
+
         }
-
-        return (productos[indice]);
-    }
-
-    getAll = async () => {
-
-        const dato = await fs.promises.readFile(nombreArchivo, `utf-8`);
-        const productos = JSON.parse(dato);
-
-        return productos;
+        catch {
+            throw new Error('Error al traer por ID');
+        }
 
     }
 
     deleteById = async (id) => {
 
-        const dato = await fs.promises.readFile(nombreArchivo, `utf-8`);
-        const productos = JSON.parse(dato);
+        const productos = this.leerArchivo();
 
-        const indice = productos.findIndex((producto) => producto.id === id);
+        try {
+            const indice = productos.findIndex((producto) => producto.id === id);
 
-        productos.splice(indice, 1);
-
-        await fs.promises.writeFile(nombreArchivo, JSON.stringify(productos, null, '\t'));
+            if (indice === -1) {
+                console.log("El producto no existe")
+            } else {
+                productos.splice(indice, 1);
+                await fs.promises.writeFile(nombreArchivo, JSON.stringify(productos, null, '\t'));
+            }
+        }
+        catch {
+            throw new Error('Error al borrar por ID');
+        }
 
     }
 
     deleteAll = async () => {
 
-        await fs.promises.writeFile(nombreArchivo, JSON.stringify([], null, '\t'));
+        try {
+            await fs.promises.writeFile(nombreArchivo, JSON.stringify([], null, '\t'));
+        }
+        catch {
+            throw new Error('Sin productos');
+        }
 
     }
 
@@ -79,33 +122,34 @@ class Contenedor {
 const contenedor1 = new Contenedor(nombreArchivo);
 
 // LLAMO TODOS LOS PRODUCTOS
-// contenedor1.getAll().then((data) => {
-//     console.log(data)
-// });
+// contenedor1.getAll()
+// .then((data) => {console.log(data)})
+// .catch(err => console.log(err))
 
 
 // LLAMO PRODUCTO POR ID
-// contenedor1.getById(2).then((data) => {
-//     console.log(data)
-// });
+// contenedor1.getById(2)
+// .then((data) => {console.log(data)})
+// .catch(err => console.log(err));
+
+
 
 
 // AGREGO UN NUEVO PRODUCTO
 // contenedor1.save({
-
 //     title: "Xiaomi Redmi",
 //     price: 12000,
+// })
+// .then((data) => { console.log(data) })
+// .catch(err => console.log(err));
 
-// }).then((data) => {
-
-//     console.log(data)
-
-// });
 
 
 // BORRO TODOS LOS PRODUCTOS
 // contenedor1.deleteAll()
+// .catch(console.log('Fallo al borrar productos'));
 
 
 // BORRO PRODUCTO POR ID
-// contenedor1.deleteById(2);
+// contenedor1.deleteById(2)
+// .catch(console.log('Fallo al borrar por ID'));
